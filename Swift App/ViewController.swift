17336a0707d789c6d1e56d9f3d0f2e2675403329
John Nyberg <nybergjd@gmail.com>
2017-07-10 13:09:12 -0400

    ok

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by John Nyberg on 7/7/17.
+//  Copyright © 2017 JohnnyGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount += 1
+        print(tapCount)
+        
+        if tapCount >= 10{
+            theLabel.text = "You tapped the button 10 times!"
+        }
+    }
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
